b7a9bb11153ed565b9d13e43da3462fa2721d3a8
Rony Nasr and Sky Rousse <sky@>
2016-06-06 15:16:34 -0700

    add script to loop through words and ouput how many occurances of each word there are

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 /*This file is for your custom js.  All yours*/
+$(function() {
+  $("form#text-block").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input#usr").val();
+    var inputArray = userInput.split(' ');
+    // alert (inputArray);
+    inputArray.forEach(function(word) {
+      var newArray = inputArray.filter(function(a) {
+        return(a === word);
+      });
+      console.log(newArray);
+      $("ul#output").append("<li>" + word + ": " + newArray.length + "</li>")
+    });
+  });
+});
